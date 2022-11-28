9cee9dfd9ed30655bcbbfd8bb64d6cc5f31caa85
redd-stk <eddieochieng11@gmail.com>
2022-11-28 05:40:05 +0300

    Small small modifications

--- teamp_app/lib/net/api_methods.dart
+++ teamp_app/lib/net/api_methods.dart
@@ -1,4 +1,3 @@
-
 import 'dart:collection';
 import 'dart:io';
 import 'package:path/path.dart' as path;
@@ -10,44 +9,45 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import '../models/products.dart';
 import '../notifier/notifier.dart';
 
-getProducts(ProductsNotifier productsNotifier) async{
-  QuerySnapshot querysnapshot = await FirebaseFirestore.instance.collection('Products').get();
+getProducts(ProductsNotifier productsNotifier) async {
+  QuerySnapshot querysnapshot =
+      await FirebaseFirestore.instance.collection('Products').get();
 
   List<Products> _productsList = [];
 
-  querysnapshot.docs.forEach((doc) { 
+  querysnapshot.docs.forEach((doc) {
     var snapData = doc.data();
     Products products = Products.fromMap(snapData as Map<String, dynamic>);
     _productsList.add(products);
   });
 
-  productsNotifier.productsList = _productsList; 
+  productsNotifier.productsList = _productsList;
 }
 
-uploadProductsAndImage(Products products, File imageFile) async{
-  if(imageFile != null){
+uploadProductsAndImage(Products products, File imageFile) async {
+  if (imageFile != null) {
     print('Uploading image');
 
     var fileExtension = path.extension(imageFile.path);
     var uuid = Uuid().v4;
-      
 
-
-    final Reference storageRef = FirebaseStorage.instance.ref().child('products/images/$uuid$fileExtension');
+    final Reference storageRef = FirebaseStorage.instance
+        .ref()
+        .child('products/images/$uuid$fileExtension');
 
     await storageRef.putFile(imageFile);
 
     String url = await storageRef.getDownloadURL();
     print("Download url: $url");
     _uploadProducts(products, imageUrl: url);
-
   }
-} 
+}
 
-_uploadProducts(Products products, {String? imageUrl}) async{
-  CollectionReference productsRef = FirebaseFirestore.instance.collection('Products');
+_uploadProducts(Products products, {String? imageUrl}) async {
+  CollectionReference productsRef =
+      FirebaseFirestore.instance.collection('Products');
 
-  if(imageUrl != null){
+  if (imageUrl != null) {
     products.image = imageUrl;
   }
 }
