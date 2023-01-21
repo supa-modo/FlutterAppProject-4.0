e04460226763fe168ccb9a50a899cbef24aa6011
redd-stk <eddieochieng11@gmail.com>
2023-01-21 04:20:38 +0300

    Modified home screen

--- teamp_app/lib/screens/homeScreen/homeScreen.dart
+++ teamp_app/lib/screens/homeScreen/homeScreen.dart
@@ -46,51 +46,6 @@ class HomeScreen extends StatelessWidget {
           )
         ],
       ),
-      // bottomNavigationBar: Container(
-      //   padding: const EdgeInsets.symmetric(vertical: 4),
-      //   decoration: const BoxDecoration(
-      //     color: Color.fromARGB(73, 230, 228, 228),
-
-      //     // boxShadow: BoxShadow(offset: Offset(0, -15))
-      //   ),
-      //   child: SafeArea(
-      //       child: Row(
-      //           mainAxisAlignment: MainAxisAlignment.spaceAround,
-      //           children: [
-      //         IconButton(
-      //             icon: SvgPicture.asset("assets/icons/Location point.svg",
-      //                 color: appPrimaryColor),
-      //             onPressed: () {}),
-      //         IconButton(
-      //             onPressed: () {
-      //               Navigator.pushNamed(context, HomeScreen.routeName);
-      //             },
-      //             icon: Icon(
-      //               Icons.home,
-      //               color: appPrimaryColor,
-      //               size: getScreenWidth(25),
-      //             )),
-      //         IconButton(
-      //             onPressed: () {
-      //               Navigator.push(context, MaterialPageRoute(builder: (context) => ProductDetails()),);
-      //             },
-      //             icon: Icon(
-      //               Icons.notifications,
-      //               color: appPrimaryColor,
-      //               size: getScreenWidth(25),
-      //             )),
-      //         // IconButton(icon: SvgPicture.asset("assets/icons/User Icon.svg", color: appPrimaryColor,), onPressed: (){}),
-      //         IconButton(
-      //             onPressed: () {
-      //               Navigator.pushNamed(context, ProfileScreen.routeName);
-      //             },
-      //             icon: Icon(
-      //               Icons.account_circle,
-      //               color: appPrimaryColor,
-      //               size: getScreenWidth(28),
-      //             )),
-      //       ])),
-      // ),
     );
   }
 }
